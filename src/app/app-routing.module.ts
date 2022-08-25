0eedff16ab326d87451c05810b8e8669e99e1f20
Thejus R <thejusr1999@gmail.com>
2022-08-25 23:24:29 +0530

    completed functions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookShelfComponent } from './components/book-shelf/book-shelf.component';
+import { EditbookComponent } from './components/editbook/editbook.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewBookComponent } from './components/new-book/new-book.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "bookShelf", component: BookShelfComponent },
+  { path: "addBook", component: NewBookComponent },
+  { path: "bookShelf/editBook/:id", component: EditbookComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
